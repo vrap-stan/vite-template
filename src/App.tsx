3211bf861d90f8a914dcbb711ce6ed42ea13366b
Stan <wjhwang@vrapoint.co.kr>
2025-02-17 09:07:28 +0900

    three + tailwind@3

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,25 @@
-import { useState } from 'react';
-import './App.css';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { OrbitControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import {
+  BrightnessContrast,
+  EffectComposer,
+} from '@react-three/postprocessing';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="w-dvw h-dvh">
+      <Canvas className="w-full h-full">
+        <mesh>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshStandardMaterial color="yellow" />
+          <OrbitControls />
+        </mesh>
+        <ambientLight />
+        <EffectComposer>
+          <BrightnessContrast brightness={0.1} contrast={0.1} />
+        </EffectComposer>
+      </Canvas>
+    </div>
   );
 }
 
